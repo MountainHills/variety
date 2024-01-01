472866bae6f7b6417b7fd2353d22c9b91ea3f368
Anton Bondoc <antonbondoc14@gmail.com>
2024-01-02 00:11:17 +0800

    chore: rename main class, add logging

---
+++ src/main/java/com/antonbondoc/VarietyApplication.java
@@ -0,0 +1,12 @@
+package com.antonbondoc;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class VarietyApplication {
+    public static void main(String[] args) {
+        log.info("Starting variety application");
+        System.out.println("Hello world!");
+    }
+}
